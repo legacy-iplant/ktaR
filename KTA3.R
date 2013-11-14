8fd482c8755629fa6a9365e107c6d5d7f3541766
Dustin Landers <dustin.landers@gmail.com>
2013-11-14 15:26:21 -0500

    added RMSE ability

--- KTA3.R
+++ KTA3.R
@@ -11,20 +11,24 @@ options(warn=-1)
 require(pROC)
 require(methods)
 
-args <- commandArgs(trailingOnly = TRUE)
-app_output_dir <- args[1]
-truth_file <- args[2]
-threshold_col <- args[3]
-SNP_col <- args[4]
+#args <- commandArgs(trailingOnly = TRUE)
+#app_output_dir <- args[1]
+#truth_file <- args[2]
+#beta_file <- args[3]
+#threshold_col <- args[4]
+#SNP_col <- args[5]
+#beta_col <- args[6]
 
-#app_output_dir <- '/Users/dustin/Desktop/DongWangOutputs'
-#truth_file <- '/Users/dustin/Desktop/dongwang_truth.txt'
-#threshold_col <- 'p'
-#SNP_col <- 'SNP'
-#beta_col <- 'Beta'
+app_output_dir <- '/Users/dustin/Desktop/DongWangOutputs'
+truth_file <- '/Users/dustin/Desktop/dongwang_truth.txt'
+beta_file <- '/Users/dustin/Desktop/dongwang_betas.txt'
+threshold_col <- 'p'
+SNP_col <- 'SNP'
+beta_col <- 'predicted'
 					 
 app_output_list <- list.files(app_output_dir) # List all files in the app output dir
 my_truth <- as.character(read.table(file=truth_file,header=FALSE,stringsAsFactor=FALSE))
+my_betas <- as.numeric(read.table(file=beta_file,header=FALSE,stringsAsFactor=FALSE))
 
 filename <- function(loc) {
 	parts <- unlist(
@@ -65,21 +69,36 @@ my_P_eval <- function(x) {
 		)
 }
 
+my_beta_eval <- function(x) {
+	return(
+		eval(parse(text=paste('mydata',beta_col,sep='$')))
+		)
+}
+
 in_truth <- function(x) {
 	x %in% my_truth
 }
 
 locs <- lapply(app_output_list, file_locations)
 
-return_data <- list()
-return_data_names <- list()
+return_auc <- list()
+return_names <- list()
+return_RMSE <- list()
 
 for (i in 1:length(locs)) {
 	mydata <- my_read_table(locs[[i]])
 	this_truth <- sapply(my_SNP_eval(locs[[i]]), in_truth)
-	return_data <- append(return_data, roc(this_truth ~ my_P_eval(locs[[i]]))$auc)
-	return_data_names <- append(return_data_names, filename(locs[[i]]))
+
+	Beta <- matrix(0,nrow=nrow(mydata),ncol=1)
+	for (j in 1:length(my_truth)) {
+		Beta[which(this_truth==TRUE)[which(names(which(this_truth==TRUE))==my_truth[j])]] <- my_betas[j]
+	}
+	return_RMSE <- append(return_RMSE, sqrt(mean((my_beta_eval(locs[[i]])-Beta)**2)))
+
+	return_auc <- append(return_auc, roc(this_truth ~ my_P_eval(locs[[i]]))$auc)
+	return_names <- append(return_names, filename(locs[[i]]))
 }
 
-Final_Data <- as.data.frame(matrix(c(unlist(return_data_names), unlist(return_data)), ncol=2))
-write(paste(Final_Data[,1], Final_Data[,2], sep='\t'), file='AUC.txt')
+write(paste(unlist(return_names), unlist(return_auc), unlist(return_RMSE), sep='\t'), file='Results.txt')
+
+#which(this_truth==TRUE)[which(names(which(this_truth==TRUE))==my_truth[1])]
