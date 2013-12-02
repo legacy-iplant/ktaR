9344afe15539fb837be2d4057098a1e0125c2efc
Dustin Landers <dustin.landers@gmail.com>
2013-12-02 11:45:47 -0500

    fixed thing

--- KTA3.R
+++ KTA3.R
@@ -11,20 +11,20 @@ options(warn=-1)
 require(pROC)
 require(methods)
 
-#args <- commandArgs(trailingOnly = TRUE)
-#app_output_dir <- args[1]
-#truth_file <- args[2]
-#beta_file <- args[3]
-#threshold_col <- args[4]
-#SNP_col <- args[5]
-#beta_col <- args[6]
+args <- commandArgs(trailingOnly = TRUE)
+app_output_dir <- args[1]
+truth_file <- args[2]
+beta_file <- args[3]
+threshold_col <- args[4]
+SNP_col <- args[5]
+beta_col <- args[6]
 
-app_output_dir <- '/Users/dustin/Desktop/DongWangOutputs'
-truth_file <- '/Users/dustin/Desktop/dongwang_truth.txt'
-beta_file <- '/Users/dustin/Desktop/dongwang_betas.txt'
-threshold_col <- 'p'
-SNP_col <- 'SNP'
-beta_col <- 'predicted'
+#app_output_dir <- '/Users/dustin/Desktop/DongWangOutputs'
+#truth_file <- '/Users/dustin/Desktop/dongwang_truth.txt'
+#beta_file <- '/Users/dustin/Desktop/dongwang_betas.txt'
+#threshold_col <- 'p'
+#SNP_col <- 'SNP'
+#beta_col <- 'predicted'
 					 
 app_output_list <- list.files(app_output_dir) # List all files in the app output dir
 my_truth <- as.character(read.table(file=truth_file,header=FALSE,stringsAsFactor=FALSE))
@@ -57,21 +57,21 @@ my_read_table <- function(x) {
 		)
 }
 
-my_SNP_eval <- function(x) {
+my_SNP_eval <- function(x='mydata') {
 	return(
-		eval(parse(text=paste('mydata',SNP_col,sep='$')))
+		eval(parse(text=paste(x,SNP_col,sep='$')))
 		)
 }
 
-my_P_eval <- function(x) {
+my_P_eval <- function(x='mydata') {
 	return(
-		eval(parse(text=paste('mydata',threshold_col,sep='$')))
+		eval(parse(text=paste(x,threshold_col,sep='$')))
 		)
 }
 
-my_beta_eval <- function(x) {
+my_beta_eval <- function(x='mydata') {
 	return(
-		eval(parse(text=paste('mydata',beta_col,sep='$')))
+		eval(parse(text=paste(x,beta_col,sep='$')))
 		)
 }
 
@@ -87,16 +87,17 @@ return_RMSE <- list()
 
 for (i in 1:length(locs)) {
 	mydata <- my_read_table(locs[[i]])
-	this_truth <- sapply(my_SNP_eval(locs[[i]]), in_truth)
+	this_truth <- sapply(my_SNP_eval('mydata'), in_truth)
 
 	Beta <- matrix(0,nrow=nrow(mydata),ncol=1)
 	for (j in 1:length(my_truth)) {
 		Beta[which(this_truth==TRUE)[which(names(which(this_truth==TRUE))==my_truth[j])]] <- my_betas[j]
 	}
-	return_RMSE <- append(return_RMSE, sqrt(mean((my_beta_eval(locs[[i]])-Beta)**2)))
 
-	return_auc <- append(return_auc, roc(this_truth ~ my_P_eval(locs[[i]]))$auc)
+	return_RMSE <- append(return_RMSE, sqrt(mean((my_beta_eval('mydata')-Beta)**2)))
+	return_auc <- append(return_auc, roc(this_truth ~ my_P_eval('mydata'))$auc)
 	return_names <- append(return_names, filename(locs[[i]]))
+
 }
 
 write(paste(unlist(return_names), unlist(return_auc), unlist(return_RMSE), sep='\t'), file='Results.txt')
