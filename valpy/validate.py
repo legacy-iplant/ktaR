8e560fcbe3ecf6e2b73e1fd4350771848504a43e
Dustin Landers <dustin.landers@gmail.com>
2014-05-23 00:23:32 -0400

    updated data class and main function

--- valpy/validate.py
+++ valpy/validate.py
@@ -15,12 +15,23 @@ def main():
 	folder, analysis, truth, snp, score, beta, filename, threshold, seper, kttype, kttypeseper = checkArgs()
 	appOutputList = checkList(getList(folder))
 	ktFile = loadKT(truth, kttypeseper)
+	#if kttype is "OTE":
+
 	for each in appOutputList:
 		acquiredData = loadFile(folder, each, seper)
 		snpColumnNo = acquiredData.header.index(snp)
 		snpColumn = list()
-		for each in acquiredData.data.keys():
-			print each
+		for each in acquiredData.data.iteritems():
+			snpColumn.append(each[1][snpColumnNo])
+		scoreColumnNo = acquiredData.header.index(score)
+		scoreColumn = list()
+		for each in acquiredData.data.iteritems():
+			scoreColumn.append(float(each[1][scoreColumnNo]))
+		if beta is not None:
+			betaColumnNo = acquiredData.header.index(beta)
+			betaColumn = list()
+			for each in acquiredData.data.iteritems():
+				betaColumn.append(float(each[1][betaColumnNo]))
 
 
 if __name__ == "__main__":
